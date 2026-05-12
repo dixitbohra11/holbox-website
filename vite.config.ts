70e474c9e0e96039c4b0708dcda0de01ea2ee99e
dixitbohra11 <dixit.bohra@finscanner.ai>
2026-05-12 22:45:43 +0530

    final vercel fix

--- vite.config.ts
+++ vite.config.ts
@@ -14,13 +14,10 @@
 //   },
 // });
 
-
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+
 import tsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
-export default defineConfig({
-  tanstackStart: {
-    server: false,
-  },
-});
+
+export default defineConfig({});
